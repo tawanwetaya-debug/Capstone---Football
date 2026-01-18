0ca322e73d9768770d56d072f6b4c0d35db319af
Tawan <tawan.wetaya@gmail.com>
2026-01-18 22:22:10 +0700

    complete API template

---
+++ src/extract/football_api/API_Team.py
@@ -0,0 +1,87 @@
+import os
+from dotenv import load_dotenv
+import requests
+from typing import Optional, Tuple, List, Dict, Any
+from src.extract.base.rate_limiter import rate_limited
+from src.extract.base.api_limits import API_SPORTS_MINUTE_LIMITER,API_SPORTS_DAILY_LIMITER
+
+
+# Fetch Team Data from Football API
+
+BASE_URL = "https://v3.football.api-sports.io"
+
+@rate_limited(API_SPORTS_DAILY_LIMITER)
+@rate_limited(API_SPORTS_MINUTE_LIMITER)
+def fetch_team_ID_from_League(league_id: int, season: int) -> Optional[Dict[str, Any]]:
+    load_dotenv()
+    api_key = os.getenv("FOOTBALL_API_KEY")
+    if not api_key:
+        raise ValueError("API key not found. Please set FOOTBALL_API_KEY in your environment variables.")
+    url = f"{BASE_URL}/teams"
+    headers = {
+        "x-apisports-key": api_key
+    }   
+    params = {
+        "league": league_id,
+        "season": season
+    }
+    response = requests.get(url, headers=headers, params=params,timeout=30)
+    if response.status_code == 200:
+        return response.json()
+        print(f'Beginning fetched data for league {league_id}{season}')
+    else:
+        print(f"Error fetching data for league {league_id}: {response.status_code} - {response.text}")
+        return None
+    
+@rate_limited(API_SPORTS_DAILY_LIMITER)
+@rate_limited(API_SPORTS_MINUTE_LIMITER)
+def fetch_team_data(team_id: int, season: int) -> Optional[Dict[str, Any]]:
+    load_dotenv()
+    if not api_key:
+        raise ValueError("API key not found. Please set FOOTBALL_API_KEY in your environment variables.")
+    api_key = os.getenv("FOOTBALL_API_KEY")
+    url = f"{BASE_URL}/teams"
+    headers = {
+        "x-apisports-key": api_key
+    }
+    params = {
+        "season": season
+    }
+
+    response = requests.get(url, headers=headers, params=params,timeout=30)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching data for team {team_id}: {response.status_code} - {response.text}")
+        return None
+
+@rate_limited(API_SPORTS_DAILY_LIMITER)
+@rate_limited(API_SPORTS_MINUTE_LIMITER)
+def fetch_team_statistics(team_id: int, season: int, league_id: int, date: Optional[str] = None) -> Optional[Dict[str, Any]]:
+    load_dotenv()
+    if not api_key:
+        raise ValueError("API key not found. Please set FOOTBALL_API_KEY in your environment variables.")
+    api_key = os.getenv("FOOTBALL_API_KEY")
+    url = f"{BASE_URL}/teams/statistics"
+    headers = {
+        "x-apisports-key": api_key
+    }
+    params = {
+        "season": season,
+        "team": team_id,
+        "league": league_id,
+
+    }
+
+    if date:
+        params['date'] = date
+
+    response = requests.get(url, headers=headers, params=params,timeout=30)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching statistics for team {team_id}: {response.status_code} - {response.text}")
+        return None
+
--- src/extract/football_api/api_transfer.py
+++ src/extract/football_api/api_transfer.py
@@ -6,50 +6,51 @@ from src.extract.base.rate_limiter import rate_limited
 from src.extract.base.api_limits import API_SPORTS_MINUTE_LIMITER,API_SPORTS_DAILY_LIMITER
 
 BASE_URL = "https://v3.football.api-sports.io"
-# Fetch Player from team Squad from Football API
+# Fetch Team Transfers from Football API
 @rate_limited(API_SPORTS_DAILY_LIMITER)
 @rate_limited(API_SPORTS_MINUTE_LIMITER)
-def fetch_team_squad(team_id: int, season: int, league_id: int) -> Optional[Dict[str, Any]]:
+def fetch_team_transfer(team_id: int) -> Dict[str, Any]:
     load_dotenv()
     api_key = os.getenv("FOOTBALL_API_KEY")
     if not api_key:
         raise ValueError("API key not found. Please set FOOTBALL_API_KEY in your environment variables.")
-    url = f"{BASE_URL}/players"
+    url = f"{BASE_URL}/transfers"
     headers = {
         "x-apisports-key": api_key
-    }   
-    params = {
-        "team": team_id,
-        "season": season,
-        "league": league_id
     }
+    params = {
+        "team": team_id
+    }
+
     response = requests.get(url, headers=headers, params=params)
+
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Error fetching squad for team {team_id}: {response.status_code} - {response.text}")
+        print(f"Error fetching transfers for team {team_id}: {response.status_code} - {response.text}")
         return None
-    
 
-
-#Fetch Player Trophy from Football API
+BASE_URL = "https://v3.football.api-sports.io"
+# Fetch Team Transfers from Football API
 @rate_limited(API_SPORTS_DAILY_LIMITER)
 @rate_limited(API_SPORTS_MINUTE_LIMITER)
-def fetch_player_trophies(player_id: int) -> Optional[Dict[str, Any]]:
+def fetch_player_transfer(player_id: int) -> Dict[str, Any]:
     load_dotenv()
     api_key = os.getenv("FOOTBALL_API_KEY")
     if not api_key:
         raise ValueError("API key not found. Please set FOOTBALL_API_KEY in your environment variables.")
-    url = f"{BASE_URL}/players/trophies"
+    url = f"{BASE_URL}/transfers"
     headers = {
         "x-apisports-key": api_key
-    }   
+    }
     params = {
         "player": player_id
     }
+
     response = requests.get(url, headers=headers, params=params)
+
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Error fetching trophies for player {player_id}: {response.status_code} - {response.text}")
+        print(f"Error fetching transfers for player {player_id}: {response.status_code} - {response.text}")
         return None
